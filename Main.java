07c026a842a2aa79e63a61c2ec18137fdcefbafa
Rachel Wu <60424265+rachelkwu@users.noreply.github.com>
2020-08-13 13:48:10 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main { 
+    public static void main(String[] args) 
+    { 
+    	System.out.println("Hello World");
+    	System.out.println("Hi, my name is Rachel!");
+    	System.out.println("I learned Python this year and coded 3 larger projects, and now I'm excited for AP CS!");
+    	System.out.println("I do competitive debate and journalism at school!");
+    	System.out.println("I like to read Wikipedia and NYT articles, oil paint, and make sculptures using clay.");
+    	System.out.println("I'm also interested in anthropology/philosophy.");
+    } 
+} 
